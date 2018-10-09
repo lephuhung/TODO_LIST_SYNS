5c99468471555153ef23e626fc5607e6f86e0dbf
lephuhung <lephuhung77@gmail.com>
2018-10-09 22:34:19 +0700

    chỉnh ant design
    setup hai input cùng hàng

--- resources/assets/js/components/Components/FormKhachHang.js
+++ resources/assets/js/components/Components/FormKhachHang.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Input, Select, Button,Col } from 'antd';
+import { Form, Input, Select, Button, Col } from 'antd';
 const FormItem = Form.Item;
 const Option = Select.Option;
 class FormKhach extends React.Component {
@@ -8,9 +8,16 @@ class FormKhach extends React.Component {
         const { getFieldDecorator } = this.props.form;
         return (
             <Form >
+                <FormItem label="Price">
+                    {getFieldDecorator('price', {
+                        initialValue: { number: 0, currency: 'rmb' },
+                        rules: [{ validator: this.checkPrice }],
+                    })(<PriceInput />)}
+                </FormItem>
                 <FormItem
                     label="Tên Khách Hàng"
-                     wrapperCol={{ span: 12 }}
+                    labelCol={{ span: 6 }}
+                    wrapperCol={{ span: 7 }}
                 >
                     {getFieldDecorator('name', {
                         rules: [{ required: true, message: 'Nhập mã đơn hàng!' }],
@@ -20,26 +27,25 @@ class FormKhach extends React.Component {
                 </FormItem>
                 <FormItem
                     label="Gender"
-                    //labelCol={{ span: 5 }}
-                    //wrapperCol={{ span: 5 }}
+                    labelCol={{ span: 5 }}
+                    wrapperCol={{ span: 10 }}
                 >
-                <span>
-                    {getFieldDecorator('gender', {
-                        rules: [{ required: true, message: 'Please select your gender!' }],
-                    })(
-                        <Select
-                            placeholder="Select a option and change input text above"
-                            onChange={this.handleSelectChange}
-                        >
-                            <Option value="male">male</Option>
-                            <Option value="female">female</Option>
-                        </Select>
-                    )}
-                    <Input
+                    <span>
+                        {getFieldDecorator('gender', {
+                            rules: [{ required: true, message: 'Please select your gender!' }],
+                        })(
+                            <Select
+                                placeholder="Select a option and change input text above"
+                                onChange={this.handleSelectChange}
+                            >
+                                <Option value="male">male</Option>
+                                <Option value="female">female</Option>
+                            </Select>
+                        )}
+                        <Input
                             type="text"
                             placeholder='Số điện thoại'
                             onChange={this.handleNumberChange}
-                            style={{ width: '65%', marginRight: '3%' }}
                         />
                     </span>
                 </FormItem>
@@ -52,7 +58,7 @@ class FormKhach extends React.Component {
                             style={{ width: '35%', marginRight: '3%' }}
                         />
                         <Select
-                            
+
                             style={{ width: '32%' }}
                         >
                             <Option value="rmb">RMB</Option>
@@ -80,3 +86,72 @@ const mapDispatchToProps = dispatch => ({
 })
 const FormKhachHang = Form.create()(FormKhach);
 export default FormKhachHang;
+class PriceInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const value = props.value || {};
+        this.state = {
+            number: value.number || 0,
+            currency: value.currency || 'rmb',
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        // Should be a controlled component.
+        if ('value' in nextProps) {
+            const value = nextProps.value;
+            this.setState(value);
+        }
+    }
+
+    handleNumberChange(e){
+        const number = parseInt(e.target.value || 0, 10);
+        if (isNaN(number)) {
+            return;
+        }
+        if (!('value' in this.props)) {
+            this.setState({ number });
+        }
+        this.triggerChange({ number });
+    }
+
+    handleCurrencyChange(currency){
+        if (!('value' in this.props)) {
+            this.setState({ currency });
+        }
+        this.triggerChange({ currency });
+    }
+
+    triggerChange (changedValue){
+        // Should provide an event to pass value to Form.
+        const onChange = this.props.onChange;
+        if (onChange) {
+            onChange(Object.assign({}, this.state, changedValue));
+        }
+    }
+
+    render() {
+        const { size } = this.props;
+        const state = this.state;
+        return (
+            <span>
+                <Input
+                    type="text"
+                    size={size}
+                    value={state.number}
+                    onChange={this.handleNumberChange}
+                    style={{ width: '65%', marginRight: '3%' }}
+                />
+                <Input
+                    type="text"
+                    size={size}
+                    value={state.number}
+                    onChange={this.handleNumberChange}
+                    style={{ width: '32%' }}
+                />
+            </span>
+        );
+    }
+}
+
